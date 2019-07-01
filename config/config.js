633828cf027dbbdd066788331376b7a8805d5c06
josonking <wangsanmu5@163.com>
2019-07-01 23:31:51 +0800

    add model

--- config/config.js
+++ config/config.js
@@ -17,12 +17,16 @@ export default {
         path: '/helloworld',
         component: 'Helloworld'
       },
+      { 
+        path: '/puzzlecards', 
+        component: 'puzzlecards' 
+      },
       {
         path: '/dashboard',
         routes: [
           { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
           { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
-          { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+          { path: '/dashboard/workplace', component: 'Dashboard/Workplace' },
         ]
       },
     ]
---
+++ src/pages/puzzlecards.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Card, Button } from 'antd';
+import { connect } from 'dva';
+
+const mapStateToProps = (state) => {
+  const cardList = state.puzzlecards;
+  return {
+    cardList,
+  };
+};
+
+@connect(mapStateToProps)
+export default class PuzzleCardsPage extends Component {
+  render() {
+    return (
+      <div>
+        {
+          !!this.props.cardList && this.props.cardList.map(card => {
+            return (
+              <Card key={card.id}>
+                <div>Q: {card.setup}</div>
+                <div>
+                  <strong>A: {card.punchline}</strong>
+                </div>
+              </Card>
+            );
+          })
+        }
+        {/* <div>
+          <Button onClick={this.addNewCard}> 添加卡片 </Button>
+        </div> */}
+      </div>
+    );
+  }
+}
